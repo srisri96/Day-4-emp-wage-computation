a1f88b3308159708502468d59347250e2593e4a1
srisri96 <108675797+srisri96@users.noreply.github.com>
2022-07-18 16:39:24 +0530

    Add files via upload

---
+++ Emp1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee4
+{
+    public class Emp1
+    {
+        public void wage()
+        {
+            int Full_Time = 1;
+            Random random = new Random();
+            int attendCheck = random.Next(0, 2);
+            if (attendCheck == Full_Time)
+            {
+                Console.WriteLine("The Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is absent");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using Employee4;
+Console.WriteLine("Employee wage computation program");
+Emp1 emp = new Emp1();
+emp.wage();
